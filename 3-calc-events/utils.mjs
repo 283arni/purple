d3d9eb2a69d2fe3f8fb224ebefc1520726de5348
283arni <35308601+283arni@users.noreply.github.com>
2024-12-12 18:39:31 +0300

    Revert "Dz 4"

---
+++ 3-calc-events/utils.mjs
@@ -0,0 +1,17 @@
+const divide = (num1, num2) => {
+    console.log(num1 - num2);
+}
+
+const multiply = (num1, num2) => {
+    console.log(num1 * num2);
+}
+
+const  add = (num1, num2) =>  {
+    console.log(num1 + num2);
+}
+
+const subtract = (num1, num2) => {
+    console.log(num1 / num2);
+}
+
+export { divide, multiply, add, subtract }
