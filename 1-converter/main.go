e338ebd275f3e7e18cdde879f71a75d498450b8c
Александр Арнаутов <282arni@gmail.com>
2025-04-01 15:29:01 +0300

    На проверку

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,30 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func scanInput() (int, string, string) {
-	var sum int
-	var firstM, secondM string
-	fmt.Println("Введите сумму денег:")
-	fmt.Scan(&sum)
-	fmt.Println("Введите из какой валюты:")
-	fmt.Scan(&firstM)
-	fmt.Println("Введите в какую валюту:")
-	fmt.Scan(&secondM)
+const USD float64 = 87.50
+const EUR float64 = 92.20
+const RUB float64 = 1
 
-	return sum, firstM, secondM
+func scanInput() (float64, float64, float64) {
+	var firstM, secondM, sum string
+	var numSum float64
+	var a, b float64
+
+	for {
+		fmt.Println("Введите сумму денег:")
+		fmt.Scan(&sum)
+		num, err := strconv.Atoi(sum)
+		numSum = float64(num)
+
+		if err != nil {
+			fmt.Println("Неправельный ввод")
+			continue
+		}
+
+		break
+	}
+
+	for {
+		fmt.Println("Введите из какой валюты (USD, EUR, RUB):")
+		fmt.Scan(&firstM)
+
+		if firstM != "USD" && firstM != "EUR" && firstM != "RUB" {
+			fmt.Println("Неправельный ввод")
+			continue
+		}
+
+		if firstM == "USD" {
+			a = USD
+		} else if firstM == "EUR" {
+			a = EUR
+		} else {
+			a = RUB
+		}
+
+		break
+	}
+
+	for {
+		fmt.Println("Введите в какую валюту (USD, EUR, RUB):")
+		fmt.Scan(&secondM)
+
+		if secondM != "USD" && secondM != "EUR" && secondM != "RUB" {
+			fmt.Println("Неправельный ввод")
+			continue
+		}
+
+		if firstM == secondM {
+			fmt.Println("Совпадение названий валют")
+			continue
+		}
+
+		if secondM == "USD" {
+			b = USD
+		} else if secondM == "EUR" {
+			b = EUR
+		} else {
+			b = RUB
+		}
+
+		break
+	}
+
+	return numSum, a, b
 }
 
-func calculate(sum int, a string, b string) float64 {
-	return 123.4
+func calculate(numSum float64, a float64, b float64) float64 {
+	return numSum * a / b
 }
 
 func main() {
-	const EUR float64 = 0.92
-	const RUB float64 = 84.67
-
-	sum, firstM, secondM := scanInput()
-	res := calculate(sum, firstM, secondM)
+	numSum, a, b := scanInput()
+	res := calculate(numSum, a, b)
 
 	fmt.Printf("%.2f", res)
 }
