db47842d153a7324a322125d5f8e89bc594f0218
Александр Арнаутов <282arni@gmail.com>
2025-04-01 13:40:40 +0300

    Добавление ввода данных

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
+func scanInput() (int, string, string) {
+	var sum int
+	var firstM, secondM string
+	fmt.Println("Введите сумму денег:")
+	fmt.Scan(&sum)
+	fmt.Println("Введите из какой валюты:")
+	fmt.Scan(&firstM)
+	fmt.Println("Введите в какую валюту:")
+	fmt.Scan(&secondM)
+
+	return sum, firstM, secondM
+}
+
+func calculate(sum int, a string, b string) float64 {
+	return 123.4
+}
+
 func main() {
 	const EUR float64 = 0.92
 	const RUB float64 = 84.67
-	sum := RUB / EUR
 
-	fmt.Printf("%.2f", sum)
+	sum, firstM, secondM := scanInput()
+	res := calculate(sum, firstM, secondM)
+
+	fmt.Printf("%.2f", res)
 }
