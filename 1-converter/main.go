a68b761654f630d1b3eec9ccde14307e225f781e
Александр Арнаутов <282arni@gmail.com>
2025-04-09 09:50:50 +0300

    правки дз 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -8,10 +8,9 @@ import (
 
 var money = map[string]float64{"USD": 87.50, "EUR": 92.20, "RUB": 1}
 
-func scanInput() (float64, float64, float64) {
+func scanInput() (float64, string, string) {
 	var firstM, secondM, sum string
 	var numSum float64
-	var a, b float64
 
 	for {
 		fmt.Println("Введите сумму денег:")
@@ -31,14 +30,12 @@ func scanInput() (float64, float64, float64) {
 		fmt.Println("Введите из какой валюты (USD, EUR, RUB):")
 		fmt.Scan(&firstM)
 
-		val, ok := money[firstM]
+		_, ok := money[firstM]
 
 		if !ok {
 			fmt.Println("Введите валюту из предложеного перед вводом")
 			continue
 		}
-
-		a = val
 
 		break
 	}
@@ -47,7 +44,7 @@ func scanInput() (float64, float64, float64) {
 		fmt.Println("Введите в какую валюту (USD, EUR, RUB):")
 		fmt.Scan(&secondM)
 
-		val, ok := money[secondM]
+		_, ok := money[secondM]
 
 		if !ok {
 			fmt.Println("Введите валюту из предложеного перед вводом")
@@ -59,25 +56,23 @@ func scanInput() (float64, float64, float64) {
 			continue
 		}
 
-		b = val
-
 		break
 	}
 
-	return numSum, a, b
+	return numSum, firstM, secondM
 }
 
-func calculate(numSum float64, a float64, b float64) (float64, error) {
+func calculate(numSum float64, a string, b string) (float64, error) {
 
 	if numSum <= 0 {
 		return 0, errors.New("Сумма денег должна быть больше 0. Попробуйте заново.")
 	}
 
-	if a <= 0 || b <= 0 {
+	if money[a] <= 0 || money[b] <= 0 {
 		return 0, errors.New("Валюта не может быть меньше или равняться 0. Проверьте правильность курса")
 	}
 
-	return numSum * a / b, nil
+	return numSum * money[a] / money[b], nil
 }
 
 func main() {
